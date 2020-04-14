88f3094a4173b1cc4595bbc7605a620557100d90
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-14 16:15:46 -0700

    finished with the useEffect hook in useDarkMode.js

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import useLocalStorage from "./useLocalStorage";
+import { useEffect } from "react";
+
+export const useDarkMode = (initValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(initValue);
+
+  useEffect(() => {
+    darkMode === true
+      ? document.querySelector("body").classList.add(dark-mode)
+      : document.querySelector("body").classList.remove(dark-mode);
+  });
+
+  return [darkMode, setDarkMode]
+};
