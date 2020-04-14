ae35493da1df870830323ded5768d5fbaa2b516a
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-14 16:40:18 -0700

    mvp

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,14 +1,14 @@
-import {useLocalStorage} from "./useLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
 import { useEffect } from "react";
 
 export const useDarkMode = (initValue) => {
-  const [darkMode, setDarkMode] = useLocalStorage(initValue);
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initValue);
 
   useEffect(() => {
     darkMode === true
-      ? document.querySelector("body").classList.add(dark-mode)
-      : document.querySelector("body").classList.remove(dark-mode);
+      ? document.querySelector("body").classList.add('dark-mode')
+      : document.querySelector("body").classList.remove('dark-mode');
   });
 
-  return [darkMode, setDarkMode]
+  return [darkMode, setDarkMode];
 };
