5e9e1087fdd5471dc5cbfed6dc4834702bb53f08
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-14 15:22:43 -0700

    finished with setValue in localStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,15 @@
 import React, {useState} from 'react';
 
 export const useLocalStorage = (key, initValue) => {
-    
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initValue;
+    });
+
+    return [storedValue];
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
 };
